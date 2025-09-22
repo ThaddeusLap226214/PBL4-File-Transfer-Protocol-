a57f3bd64d2b4519e38255bec1e5d53d57f9bdb9
Nguyen Thanh Lap <nguyenthanhlap205@gmail.com>
2025-09-22 08:45:11 +0700

    client gửi tên file và server gửi file, các file đặt trong thư mục filePath được tạo trước đó ở ngoài máy tính, thư mục này cùng cấp với src của project.

---
+++ src/Block1/Client_test.java
@@ -0,0 +1,61 @@
+package Block1;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client_test implements Runnable{
+	Socket soc;
+	
+	static public void main(String[] args) {
+		new Client_test();
+	}
+	
+	public Client_test() {
+		try {
+			soc = new Socket("localhost", 5000);
+			Thread x = new Thread(this);
+			x.start();
+		}catch(IOException e) {
+			System.out.println("Lỗi kết nối " + e.getMessage());
+		}
+	}
+	
+	@Override
+	public void run() {
+		try {
+			DataInputStream dis = new DataInputStream(soc.getInputStream());
+			DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
+			String mes = dis.readUTF();
+			System.out.println(mes);
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String nameFile = br.readLine();
+			dos.writeUTF(nameFile);
+			System.out.println(dis.readUTF());
+			
+			long fileSize = dis.readLong();
+			
+			FileOutputStream fileOut = new FileOutputStream("filePath\\gift");
+			byte[] buffer = new byte[1024*4];
+			long totalRead = 0;
+			int bytesRead;
+			
+			while(totalRead < fileSize && (bytesRead = dis.read(buffer)) != -1) {
+				fileOut.write(buffer, 0, bytesRead);
+				totalRead += bytesRead;
+			}
+			fileOut.close();
+			System.out.println("Done");
+			
+		} catch (IOException e) {
+			System.out.println("Không lấy được luồng dữ liệu từ socket " + e.getMessage());
+		}
+	}
+}
---
+++ src/Block1/ServerFTP.java
@@ -0,0 +1,69 @@
+package Block1;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerFTP {
+
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(5000);
+			while(true) {
+				Socket soc = server.accept();
+				System.out.println("Client connected.");
+				Xuly x = new Xuly(soc);
+				x.start();
+			}
+		} catch(IOException e) {
+			System.out.println("Lỗi kết nối " + e.getMessage());
+		}
+	}
+
+}
+
+class Xuly extends Thread{
+	Socket soc;
+	public Xuly(Socket soc) {
+		this.soc = soc;
+	}
+	@Override
+	public void run() {
+		try {
+			DataInputStream dis = new DataInputStream(soc.getInputStream());
+			DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
+			dos.writeUTF("Hi Client, please send name file\n");
+			String nameFile = dis.readUTF();
+			dos.writeUTF("I will send to you file " + nameFile + "\n");
+			
+			String filePath = "filePath\\" + nameFile;
+			File file = new File(filePath);
+			FileInputStream fileIn = new FileInputStream(file);
+			
+			dos.writeLong(file.length());
+			
+			byte[] buffer = new byte[1024*4];
+			int bytesRead;
+			
+			while((bytesRead = fileIn.read(buffer)) != -1) {
+				dos.write(buffer, 0, bytesRead);;
+			}
+			
+			fileIn.close();
+			dos.flush();
+			System.out.println("file is send to client");
+			
+		} catch (IOException e) {
+			System.out.println("Không lấy được luồng dữ liệu từ socket " + e.getMessage());;
+		}
+	}
+}
