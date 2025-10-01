d498ad7dcefed9a64391cb2d0a45ee9159ded1b9
Nguyen Thanh Lap <nguyenthanhlap205@gmail.com>
2025-10-01 22:12:42 +0700

    CLient nhận hoặc gửi file bất kì nhiều lần

--- src/Block1/Client_test.java
+++ src/Block1/Client_test.java
@@ -5,6 +5,8 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -28,40 +30,93 @@ public class Client_test implements Runnable{
 		}
 	}
 	
+	public void sendFile(DataInputStream dis, DataOutputStream dos, String nameFile) {
+		try {
+			String filePath = "filePath\\" + nameFile;
+			File file = new File(filePath);
+			FileInputStream fileIn = new FileInputStream(file);
+			
+			dos.writeLong(file.length());
+			
+			byte[] buffer = new byte[1024*4];
+			int bytesRead;
+			
+			while((bytesRead = fileIn.read(buffer)) != -1) {
+				dos.write(buffer, 0, bytesRead);;
+			}
+			
+			fileIn.close();
+			dos.flush();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void reiceveFile(DataInputStream dis, DataOutputStream dos, String nameFile, String reiceve) {
+		//Client gửi tên file sau đó đợi nhận độ lớn file
+		try {
+			dos.writeUTF(nameFile);
+			long fileSize = dis.readLong();
+			
+			FileOutputStream fileOut = new FileOutputStream("filePath\\" + reiceve);
+			byte[] buffer = new byte[1024*4];
+			long totalRead = 0;
+			int bytesRead;
+			
+			while(totalRead < fileSize && (bytesRead = dis.read(buffer)) != -1) {
+				fileOut.write(buffer, 0, bytesRead);
+				totalRead += bytesRead;
+			}
+			fileOut.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public void run() {
 		try {
+			DataInputStream dis = new DataInputStream(soc.getInputStream());
+			DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			while(true) {
-				DataInputStream dis = new DataInputStream(soc.getInputStream());
-				DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
-				String mes = dis.readUTF();
-				System.out.println(mes);
-				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				String nameFile = br.readLine();
-				if(nameFile.equalsIgnoreCase("delete")) {
-					System.out.println("Thank you.");
-					break;
+				System.out.print("Enter '1' to receive file and other to send file: ");
+				String seOrRe = br.readLine();
+				if(seOrRe.equals("1")) { // 1 là client yêu cầu file
+					dos.writeUTF(seOrRe);
+					System.out.print("Name file: ");
+					String nameFile = br.readLine();
+					if(nameFile.equalsIgnoreCase("delete")) {
+						System.out.println("Thank.");
+						break;
+					}
+					System.out.print("Receive: ");
+					String reiceve = br.readLine();
+					
+					reiceveFile(dis, dos, nameFile, reiceve);
+					
+					System.out.println("Done");
+				}else { // client gửi file cho server
+					dos.writeUTF("-1");
+					System.out.print("Name file: ");
+					String nameFile = br.readLine();
+					if(nameFile.equalsIgnoreCase("delete")) {
+						System.out.println("Thank.");
+						break;
+					}
+					System.out.print("send with name: ");
+					String reiceve = br.readLine();
+					
+					//server sẽ cần tên file để lưu ra ngoài
+					dos.writeUTF(reiceve);
+					
+					sendFile(dis, dos, nameFile);
+					
+					System.out.println("file is send to server");
 				}
-				System.out.print("you want the file send to:");
-				String receive = br.readLine();
-				dos.writeUTF(nameFile);
-				System.out.println(dis.readUTF());
+
 				
-				long fileSize = dis.readLong();
-				
-				FileOutputStream fileOut = new FileOutputStream("filePath\\" + receive);
-				byte[] buffer = new byte[1024*4];
-				long totalRead = 0;
-				int bytesRead;
-				
-				while(totalRead < fileSize && (bytesRead = dis.read(buffer)) != -1) {
-					fileOut.write(buffer, 0, bytesRead);
-					totalRead += bytesRead;
-				}
-				fileOut.close();
-				System.out.println("Done");
-				
-			}			
+			}
 		} catch (IOException e) {
 			System.out.println("Không lấy được luồng dữ liệu từ socket " + e.getMessage());
 		}
--- src/Block1/ServerFTP.java
+++ src/Block1/ServerFTP.java
@@ -7,6 +7,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -36,35 +37,70 @@ class Xuly extends Thread{
 	public Xuly(Socket soc) {
 		this.soc = soc;
 	}
+	public void sendFile(DataInputStream dis, DataOutputStream dos, String nameFile) {
+		try {
+			String filePath = "filePath\\" + nameFile;
+			File file = new File(filePath);
+			FileInputStream fileIn = new FileInputStream(file);
+			
+			dos.writeLong(file.length());
+			
+			byte[] buffer = new byte[1024*4];
+			int bytesRead;
+			
+			while((bytesRead = fileIn.read(buffer)) != -1) {
+				dos.write(buffer, 0, bytesRead);;
+			}
+			
+			fileIn.close();
+			dos.flush();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void reiceveFile(DataInputStream dis, DataOutputStream dos, String reiceve) {
+		//Client gửi tên file sau đó đợi nhận độ lớn file
+		try {
+			long fileSize = dis.readLong();
+			
+			FileOutputStream fileOut = new FileOutputStream("filePath\\" + reiceve);
+			byte[] buffer = new byte[1024*4];
+			long totalRead = 0;
+			int bytesRead;
+			
+			while(totalRead < fileSize && (bytesRead = dis.read(buffer)) != -1) {
+				fileOut.write(buffer, 0, bytesRead);
+				totalRead += bytesRead;
+			}
+			fileOut.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 	@Override
 	public void run() {
 		try {
+			DataInputStream dis = new DataInputStream(soc.getInputStream());
+			DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
 			while(true) {
-				DataInputStream dis = new DataInputStream(soc.getInputStream());
-				DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
-				dos.writeUTF("Hi Client, please send name file");
-				String nameFile = dis.readUTF();
-				dos.writeUTF("I will send to you file " + nameFile + "\n");
-				
-				String filePath = "filePath\\" + nameFile;
-				File file = new File(filePath);
-				FileInputStream fileIn = new FileInputStream(file);
-				
-				dos.writeLong(file.length());
-				
-				byte[] buffer = new byte[1024*4];
-				int bytesRead;
-				
-				while((bytesRead = fileIn.read(buffer)) != -1) {
-					dos.write(buffer, 0, bytesRead);;
+				String seOrRe = dis.readUTF();
+				if(seOrRe.equals("1")) { // 1 là client yêu cầu file
+					String nameFile = dis.readUTF();
+					
+					sendFile(dis, dos, nameFile);
+					
+					System.out.println("file is send to client");	
 				}
-				
-				fileIn.close();
-				dos.flush();
-				System.out.println("file is send to client");				
+				else { // còn lại là client gửi file cho server
+					
+					String reiceve = dis.readUTF();
+					
+					reiceveFile(dis, dos, reiceve);
+					
+				}
 			}
-		} catch (IOException e) {
-			System.out.println("Không lấy được luồng dữ liệu từ socket " + e.getMessage());;
+		}catch(IOException e) {
+			System.out.println("Không lấy được luồng dữ liệu" + e.getMessage());
 		}
 	}
 }
